56f7e32e6ac2ec3fbb35c0c422aab809ba813ab4
Justyna Malinowska <justynamalinowska758@gmail.com>
2025-01-02 19:10:00 +0100

    Init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+};
